28766634d94d8a7b2f747df7cd419699d7a76dc7
Emin Lu <luchenming727@gmail.com>
2026-07-23 15:14:14 +0800

    双端队列（数组）
    此代码是用数组实现双端队列的方法，在少量数据下，会比双向链表更快，因为leetcode此题给的数据量小，所以这种方法是更快的方法只用了1ms，双向链表的话是4ms

--- 双端队列.cpp
+++ 双端队列.cpp
@@ -1,117 +1,98 @@
 
 //https://leetcode.cn/problems/design-circular-deque/description/
 public class MyCircularDeque {
-    LinkedList<int> list = new LinkedList<int>();
-    int size;
-    int limit;
-
-
+    private int[] a;
+    private int l;     // 头指针 (Front)
+    private int r;     // 尾指针 (Rear)
+    private int size;  // 当前元素个数
+    private int limit; // 队列最大容量
 
     public MyCircularDeque(int k) {
-        limit=k;
-        size=0;
-        
+        a = new int[k];
+        l = 0;
+        r = 0;
+        size = 0;
+        limit = k;
     }
     
     public bool InsertFront(int value) {
-        if(IsFull())
-        {
-            return false;
+        if (IsFull()) return false;
+
+        a[l] = value;
+        size++;
+
+        if (l == 0) {
+            l = limit - 1;
+        } else {
+            l--;
         }
-        else
-        {
-            list.AddFirst(value);
-            size++;
-            return true;
-        }
-        
+        return true;
     }
-    
+
     public bool InsertLast(int value) {
-          if(IsFull())
+        if (IsFull()) return false;
+        else if(r==limit-1)
         {
-            return false;
-        }
-        else
-        {
-            list.AddLast(value);
+            r=0;
+            a[r]=value;
             size++;
             return true;
         }
-        
+        else
+        {
+            r++;
+            a[r]=value;
+            size++;
+            return true;
+        }
     }
     
     public bool DeleteFront() {
-        if(IsEmpty())
-        {
-            return false;
+        if (IsEmpty()) return false;
+
+        if (l == limit - 1) {
+            l = 0;
+        } else {
+            l++;
         }
-        else
-        {
-            list.RemoveFirst();
-            size--;
-            return true;
-        }
-        
+        size--;
+        return true;
     }
     
     public bool DeleteLast() {
-         if(IsEmpty())
-        {
-            return false;
+        if (IsEmpty()) return false;
+
+        if (r == 0) {
+            r = limit - 1;
+        } else {
+            r--;
         }
-        else
-        {
-            list.RemoveLast();
-            size--;
-            return true;
-        }
-        
+        size--;
+        return true;
     }
     
     public int GetFront() {
-        if(IsEmpty())
-        {
-            return -1;
-        }
-        else
-        {
-            int x=list.First.Value;
-            return x;
-        }
+        if (IsEmpty()) return -1;
 
-        
+        if (l == limit - 1) {
+            return a[0]; 
+        } else {
+            return a[l + 1];
+        }
     }
     
     public int GetRear() {
-        if(IsEmpty())
-        {
-            return -1;
-        }
-        else
-        {
-            int x=list.Last.Value;
-            return x;
-        }
+        if (IsEmpty()) return -1;
 
-        
+        // 修正：r 为 0 时尾巴在 limit - 1
+        return a[r];
     }
     
     public bool IsEmpty() {
-        if(size==0)
-        return true;
-        else
-        return false;
-        
+        return size == 0;
     }
     
     public bool IsFull() {
-        if(size==limit)
-        {
-            return true;
-        }
-        else
-        return false;
-        
+        return size == limit;
     }
 }
